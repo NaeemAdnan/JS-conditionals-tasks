ab471b99824636b74dc97874386432918a6d3097
NaeemAdnan <naeem.cse.nwu@gmail.com>
2024-02-03 03:03:31 +0600

    bmi and freedrinks add

--- bmi.js
+++ bmi.js
@@ -11,3 +11,20 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
     - Otherwise, you are obese.
 
 */
+
+var weight = 80;
+var height = 1.8288;
+
+var BMI = weight / (height * height)
+ if( BMI < 18.5){
+    console.log("You are underweight.")
+ }
+ else if(BMI >= 18.5 && BMI < 24.9){
+    console.log("You are normal.")
+ }
+ else if(BMI >= 25 && BMI < 29.9){
+    console.log("You are overweight.")
+ }
+ else{
+    console.log("you are obese.")
+ }
---
+++ freedrinks.js
@@ -0,0 +1,24 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+var burger = 600 ;
+
+if ( burger > 500){
+    console.log("Free coke");
+}
+else{
+    console.log("Code is : 30tk");
+}
+
+var burger = 400 ;
+
+if ( burger > 500){
+    console.log("Free coke");
+}
+else{
+    console.log("Code is : 30tk");
+}
